5e4573a69cedbd124d39fce03a1f73dabc0173a4
keithkaykay <kaykaykeith@yahoo.com>
2021-07-02 10:53:27 +0300

    make new file

---
+++ js-kk/script.js
@@ -0,0 +1,16 @@
+function loginadmin(){
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+}
+
+function getUsername() {
+  var username = document.getElementById("username").value;
+  return username;
+}
+
+function getPassword() {
+  var password = document.getElementById("password").value;
+  return password;
+}
+
